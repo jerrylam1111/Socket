45c45099d6500f995c90ad5b8fa1b84e34cf99a1
Jerry <jerrylam1111@gmail.com>
2020-05-18 12:54:39 +1000

    Socket first commit

---
+++ app/src/main/java/com/example/socket/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.socket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText e1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1 = (EditText) findViewById(R.id.editText);
+
+        Thread myThread = new Thread(new MyServerThread());
+        myThread.start();
+
+
+
+    }
+
+    class MyServerThread implements Runnable{
+        Socket s;
+        ServerSocket ss;
+        InputStreamReader isr;
+        BufferedReader bufferedReader;
+        Handler h = new Handler();
+        String message;
+
+        @Override
+        public void run(){
+            try{
+                ss = new ServerSocket(7801);
+                while(true){
+                    s = ss.accept();
+                    isr = new InputStreamReader(s.getInputStream());
+                    bufferedReader = new BufferedReader(isr);
+                    message = bufferedReader.readLine();
+
+                    h.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send (View v){
+        Sender sender = new Sender();
+        sender.execute(e1.getText().toString());
+    }
+
+
+}
